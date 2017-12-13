f72da6b951965ad4915fc22d122b1c2df513046c
MAlDibawy <34455650+MAlDibawy@users.noreply.github.com>
2017-12-13 13:38:25 +0200

    first

---
+++ Software Project/Adwarlak/src/Customer.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Customer {
+	private float rate;
+//	private Cart cart;
+	private float voucherCard;
+	private ArrayList<Product> prevBuy;
+//	private ArrayList<Product> prevSearch;
+//	CustomerGUI gui;
+	
+	public void addToPrevBuy(Product p) {
+		if(!prevBuy.contains(p)) {
+			prevBuy.add(p);
+		}
+		else {
+			
+		}
+	}
+
+//	public void addToPrevSearch() {
+//		
+//	}
+	
+//	public void addToCart(Product p) {
+//		cart.addToBuy();
+//	}
+	
+//	public void openCart() {
+//		if(!cart.toBuy.isEmpty()) {
+//			
+//		}
+//	}
+	
+}
--- Software Project/Adwarlak/src/Guest.java
+++ Software Project/Adwarlak/src/Guest.java
@@ -1,4 +1,11 @@
 
 public class Guest extends User{
-
+//	private GUI GuestGUI;
+	public void signUpCustomer() {
+		
+	}
+	
+	public void signUpShopOwner() {
+		
+	}
 }
