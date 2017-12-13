b78d750819b7da1c1d67dd188b772c0abeba4332
MostafaAmin07 <32510728+MostafaAmin07@users.noreply.github.com>
2017-12-13 11:22:36 +0200

    Word Files

--- Software Project/Adwarlak/src/Category.java
+++ Software Project/Adwarlak/src/Category.java
@@ -1,6 +1,21 @@
 import java.util.ArrayList;
 
 public class Category {
+	//Attributes
 	private String name;
 	private ArrayList<Product> products;
+	
+	//Getters & Setters
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public ArrayList<Product> getProducts() {
+		return products;
+	}
+	public void setProducts(ArrayList<Product> products) {
+		this.products = products;
+	}	
 }
--- Software Project/Adwarlak/src/User.java
+++ Software Project/Adwarlak/src/User.java
@@ -1,7 +1,39 @@
 
 public class User {
+	// attributes
 	private String name, mail, phone;
 	private int age;
 	private Search search;
-	
+
+	//Getter  & Setter
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Search getSearch() {
+		return search;
+	}
+	public void setSearch(Search search) {
+		this.search = search;
+	}
 }
