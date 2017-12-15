bacf6add95ee6e984b4aaa55695557dd761f4763
SsooloomM <rihdmagk0111@gmail.com>
2017-12-15 16:37:55 +0200

    Database connection

---
+++ Software Project/Adwarlak/src/Database.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class Database {
+
+	public Connection conn = null;
+	private String dbName = null;
+	
+	public Database() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Database(String dbName, String dbURL){
+		this.dbName = dbName;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			this.conn = DriverManager.getConnection(dbURL+dbName, "root", "123456");//here put the new simple url.
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private ResultSet runSql(String sql) throws SQLException {
+		Statement sta = conn.createStatement();
+		return sta.executeQuery(sql);
+	}
+	
+}
