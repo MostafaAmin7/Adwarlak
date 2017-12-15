0d43d8ff7062a97aac28bcec2a359cf13389db54
shroukksobhy <shroukksobhy@gmail.com>
2017-12-15 20:51:12 +0200

    Sign in& up

--- Software Project/Adwarlak/src/Admin.java
+++ Software Project/Adwarlak/src/Admin.java
@@ -1,4 +1,13 @@
 
-public class Admin {
+public class Admin extends User{
+	private int level;
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
 	
 }
