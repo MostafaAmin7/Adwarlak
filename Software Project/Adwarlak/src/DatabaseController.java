c9f8c5d492f9fe0dbe6d367e8a7891b2d2fcc19a
SsooloomM <rihdmagk0111@gmail.com>
2017-12-15 18:07:52 +0200

    database contoller

--- Software Project/Adwarlak/src/DatabaseController.java
+++ Software Project/Adwarlak/src/DatabaseController.java
@@ -1,4 +1,14 @@
-
 public class DatabaseController {
+	
+	private Database database;
+	
+	public DatabaseController() {
+		database = new Database();
+	}
+	
+	public User signIn(String name, String password) {
+		String query = "SELECT * FROM user WHERE u_name = " + name + " and password = " + password;
+		return database.signIn(query);
+	}
 	
 }
