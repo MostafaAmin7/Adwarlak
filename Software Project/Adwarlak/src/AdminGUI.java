464f438e190c12ea81d085e23b52e1c35ddaa071
MostafaAmin07 <32510728+MostafaAmin07@users.noreply.github.com>
2017-12-16 21:20:54 +0200

    new

--- Software Project/Adwarlak/src/AdminGUI.java
+++ Software Project/Adwarlak/src/AdminGUI.java
@@ -1,17 +1,51 @@
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class AdminGUI extends JPanel{
 	//ENTRY constructors
 	public AdminGUI(Admin shopowner) {
 		instance=this;
-		owner=shopowner;
+		admin=shopowner;
 		setup();
 	}
 	//ENTRY Attributes
 	private AdminGUI instance;
-	private Admin owner;
+	private Admin admin;
 	//ENTRY Functions
 	public void setup() {
+		setLayout(null);
+		setBounds(0, 0, 525, 500);
+		SearchGUI searchGUI = new SearchGUI();
+		searchGUI.setBounds(10, 100, 500, 350);
+		add(searchGUI);
 		
+		JLabel lblAdwarlak = new JLabel("Adwarlak");
+		lblAdwarlak.setBounds(223, 11, 61, 14);
+		add(lblAdwarlak);
+		
+		JButton backButton = new JButton("Back");
+		backButton.setBounds(10, 7, 89, 23);
+		add(backButton);
+		
+		JButton logOutButton = new JButton("Log out");
+		logOutButton.setBounds(10, 66, 89, 23);
+		add(logOutButton);
+		
+		JButton addVcardButton = new JButton("Add VCard");
+		addVcardButton.setBounds(138, 66, 89, 23);
+		add(addVcardButton);
+		
+		JButton addProductButton = new JButton("Add Product");
+		addProductButton.setBounds(256, 66, 103, 23);
+		add(addProductButton);
+		
+		JButton addBrandButton = new JButton("Add Brand");
+		addBrandButton.setBounds(387, 66, 103, 23);
+		add(addBrandButton);
+		
+		JLabel AdminName = new JLabel(admin.getName());
+		AdminName.setBounds(420, 11, 46, 14);
+		add(AdminName);
 	}
 }
--- Software Project/Adwarlak/src/shopOwnerGUI.java
+++ Software Project/Adwarlak/src/shopOwnerGUI.java
@@ -1,4 +1,10 @@
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 public class ShopOwnerGUI extends JPanel{
 	//ENTRY constructors
@@ -10,8 +16,64 @@ public class ShopOwnerGUI extends JPanel{
 	//ENTRY Attributes
 	private ShopOwnerGUI instance;
 	private ShopOwner owner;
+	private JTextField textField;
+	private JTextField textField_1;
 	//ENTRY Functions
 	public void setup() {
+setLayout(null);
 		
+		SearchGUI searchGUI = new SearchGUI();
+		searchGUI.setBounds(10, 139, 500, 350);
+		add(searchGUI);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.setBounds(10, 11, 89, 23);
+		add(btnBack);
+		
+		JButton btnLogOut = new JButton("Log out");
+		btnLogOut.setBounds(10, 42, 89, 23);
+		add(btnLogOut);
+		
+		JButton btnSuggestProduct = new JButton("Suggest Product");
+		btnSuggestProduct.setBounds(10, 105, 111, 23);
+		add(btnSuggestProduct);
+		
+		JButton btnAddStore = new JButton("Add Store");
+		btnAddStore.setBounds(210, 105, 89, 23);
+		add(btnAddStore);
+		
+		JButton btnShowStatistics = new JButton("Show Statistics");
+		btnShowStatistics.setBounds(399, 105, 111, 23);
+		add(btnShowStatistics);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(399, 49, 111, 45);
+		add(scrollPane);
+		
+		JList list = new JList();
+		scrollPane.setViewportView(list);
+		
+		JLabel lblAdwarlak = new JLabel("Adwarlak");
+		lblAdwarlak.setBounds(210, 15, 65, 14);
+		add(lblAdwarlak);
+		
+		JLabel label = new JLabel("");
+		label.setBounds(429, 15, 46, 14);
+		add(label);
+		
+		JLabel lblSelectStore = new JLabel("Select Store");
+		lblSelectStore.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSelectStore.setBounds(400, 35, 111, 14);
+		add(lblSelectStore);
+		
+		textField = new JTextField();
+		textField.setBounds(140, 75, 86, 20);
+		add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(275, 75, 86, 20);
+		add(textField_1);
+		textField_1.setColumns(10);
 	}
 }
